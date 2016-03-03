214f0116f7781b0b5594ad7569366cc5b02db3a0
Danial Goodwin <danialgoodwin@gmail.com>
2016-03-03 10:06:50 -0500

    Add DateTimeUtils

---
+++ lib/src/main/java/com/danialgoodwin/android/util/DateTimeUtils.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Danial Goodwin on 2016-03-03.
+ */
+package com.danialgoodwin.android.util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+/** Helper methods related to dates and times. */
+public class DateTimeUtils {
+
+    // Note: This is not thread-safe. Depending on usage, it could be better for using ThreadLocal or inline.
+//     private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); // Could be "yyyyMMdd_HHmmss".
+
+    /* Suppress default constructor for non-instantiability. */
+    private DateTimeUtils() { throw new AssertionError(); }
+
+    /** Return the current date formatted as "yyyy-MM-dd". */
+    public static String getYearMonthDay() {
+        GregorianCalendar calendar = new GregorianCalendar();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd")
+//        sdf.format();
+        // Possibly faster than above, need to test.
+        return String.format(Locale.US, "%d-%d-%d", year, month, day);
+    }
+
+    /** Return the current time for the device locale. */
+//    public static long getTimeMillisInLocale() {
+//        GregorianCalendar calendar = new GregorianCalendar();
+//        return calendar.getTimeInMillis();
+//    }
+
+}
